385e7210a0385b1731255756c48d5efefb34da4d
ismael4174 <ouattara4174@gmail.com>
2020-07-27 13:16:48 +0000

    Update index.html

--- js/active.js
+++ js/active.js
@@ -32,6 +32,30 @@
     });
   }
 
+  // :: Partner slide
+  $('#recipeCarousel').carousel({
+    interval: 10000
+  })
+  
+  $('.carousel .carousel-item').each(function(){
+      var minPerSlide = 3;
+      var next = $(this).next();
+      if (!next.length) {
+      next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+      
+      for (var i=0;i<minPerSlide;i++) {
+          next=next.next();
+          if (!next.length) {
+            next = $(this).siblings(':first');
+          }
+          
+          next.children(':first-child').clone().appendTo($(this));
+        }
+  });
+  
+
   // :: 5.0 Sliders Active Code
   if($.fn.owlCarousel) {
     welcomeSlide.owlCarousel({
